23bb889fd01a95523264b6c81ca80d65318af6bb
Samuel Adjei <samueladj3i@gmail.com>
2020-04-19 22:32:51 +0100

    Added dark mode toggle

---
+++ js/app.js
@@ -0,0 +1,19 @@
+var checkbox = document.querySelector('input[name=theme]');
+
+checkbox.addEventListener('change', function () {
+  // if this is checked, it will toggle to dark 
+  if (this.checked) {
+    transition();
+    document.documentElement.setAttribute('data-theme', 'light');
+  } else {
+    transition();
+    document.documentElement.setAttribute('data-theme', 'dark');
+  }
+});
+
+let transition = () => {
+  document.documentElement.classList.add('transition');
+  window.setTimeout(() => {
+    document.documentElement.classList.remove('transition');
+  }, 1000);
+};
